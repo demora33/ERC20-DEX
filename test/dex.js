0e44c3dfb1ad68ac4dc88dea23912b4ca91e4676
Alfonso de Mora <alfonsodemoranava14@hotmail.es>
2022-09-23 09:49:12 +0200

    Add development networks

--- test/dex.js
+++ test/dex.js
@@ -1,14 +1,17 @@
+const { expectRevert } = require("@openzeppelin/test-helpers");
 const Dex = artifacts.require("mocks/Dex.sol");
 const Dai = artifacts.require("mocks/Dai.sol");
 const Bat = artifacts.require("mocks/Bat.sol");
 const Zrx = artifacts.require("mocks/Zrx.sol");
 const Rep = artifacts.require("mocks/Rep.sol");
 
-contract("Dex", () => {
-  let dai, bat, rep, zrx;
+contract("Dex", (accounts) => {
+  let dai, bat, rep, zrx, dex;
 
-  const [DAI, BAT, REP, ZRX] = ["DAI", "BAT", "REP", "ZRX"].map((ticker) =>
-    web3.utils.fromAscii(ticker)
+  const [trader1, trader2] = [accounts[1], accounts[2]];
+
+  const [DAI, BAT, REP, ZRX] = ["DAI", "BAT", "REP", "ZRX"].map((symbol) =>
+    web3.utils.fromAscii(symbol)
   );
 
   beforeEach(async () => {
@@ -19,7 +22,7 @@ contract("Dex", () => {
       Zrx.new(),
     ]);
 
-    const dex = await Dex.new();
+    dex = await Dex.new();
 
     await Promise.all([
       dex.addToken(DAI, dai.address),
@@ -27,5 +30,41 @@ contract("Dex", () => {
       dex.addToken(REP, rep.address),
       dex.addToken(ZRX, zrx.address),
     ]);
+
+    const amount = web3.utils.toWei("1000");
+
+    const seedTokenBalance = async (token, trader) => {
+      await token.faucet(trader, amount);
+      await token.approve(dex.address, amount, { from: trader });
+    };
+
+    await Promise.all(
+      [dai, bat, rep, zrx].map((token) => seedTokenBalance(token, trader1))
+    );
+
+    await Promise.all(
+      [dai, bat, rep, zrx].map((token) => seedTokenBalance(token, trader2))
+    );
   });
+
+  it("should deposit the correct amount of tokens", async () => {
+    const amount = await web3.utils.toWei("100");
+
+    await dex.deposit(amount, DAI, {
+      from: trader1,
+    });
+    const balance = await dex.traderBalances(trader1, DAI);
+    assert(balance.toString() === amount);
+  });
+
+  // it("should NOT deposit tokens if token does not exist", async () => {
+  //   await expectRevert(
+  //     dex.deposit(
+  //       web3.utils.toWei("100"),
+  //       web3.utils.fromAscii("TOKEN-DOES-NOT-EXIST"),
+  //       { from: trader1 }
+  //     ),
+  //     "This token does not exist"
+  //   );
+  // });
 });
